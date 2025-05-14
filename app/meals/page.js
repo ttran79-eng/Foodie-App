393e776f4442bc4462e7ca83c0b1e49588e951b9
Tin <ttran79@rrc.ca>
2025-05-14 15:18:53 -0500

    Worked on meals page/grid/item and populate db

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,5 +1,25 @@
+import classes from './page.module.css';
+import Link from 'next/link';
+import MealsGrid from '@/components/meals/meals-grid';
+
 export default function MealsPage(){
     return(
-        <h1>Meals Page</h1>
+        <>
+            <header className={classes.header}>
+                <h1>
+                    Delicious meals, created <span className={classes.highlight}>by you</span>
+                </h1>
+                <p>
+                    Choose your favourite receipt and cook it yourself. It is easy and fun!
+                </p>
+                <p className={classes.cta}>
+                    <Link href="/meals/share">Share Your Favourite Recipe</Link>
+                </p>
+            </header>
+
+            <main className={classes.main}>
+                <MealsGrid meals={[]} />
+            </main>
+        </>
     )
 }
