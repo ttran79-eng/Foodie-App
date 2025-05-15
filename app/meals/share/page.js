57170b8cece3f0e750e7dcb4d6a4262bcac2bd7e
Tin <ttran79@rrc.ca>
2025-05-15 15:04:16 -0500

    Using useFormState to repopulate form with original input when invalid data is detected

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,4 +1,6 @@
+'use client';
 
+import { useFormState } from 'react-dom';
 import classes from './page.module.css';
 import ImagePicker from '@/components/meals/image-picker';
 import MealsFormSubmit from '@/components/meals/meals-form-submit';
@@ -6,7 +8,8 @@ import { shareMeal } from '@/lib/form_submission_action';
 
 
 export default function ShareMealPage() {
-   
+    // useActionState acts as a man in the middle, it will repopulate form if it does not go through
+   const [state, formAction] = useFormState(shareMeal, {message: null} );
 
   return (
     <>
@@ -17,7 +20,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -46,6 +49,7 @@ export default function ShareMealPage() {
             ></textarea>
           </p>
           <ImagePicker label="Your image" name="image" />
+          {state.message && <p>{state.message}</p>}
           <p className={classes.actions}>
             <MealsFormSubmit />
           </p>
