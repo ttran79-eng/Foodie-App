a65fa3ab40deeac7905ced34df7d3e3a5f9c5790
Tin <ttran79@rrc.ca>
2025-05-15 11:42:34 -0500

    Implemented image picker feature + image preview feature

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -1,26 +1,56 @@
 'use client';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import classes from './image-picker.module.css'
+import Image from 'next/image';
 
 export default function ImagePicker({ label, name }) {
+    const [pickedImage, setPickedImage] = useState();
     const imageInput = useRef();
 
     function handlePickClick(){
         imageInput.current.click();
 
     }
+
+    function handleImageChange(event){
+        // This access the first files
+        const file = event.target.files[0];
+
+        if (!file){
+            return;
+        }
+        const fileReader = new FileReader();
+
+        fileReader.onload = () => {
+            setPickedImage(fileReader.result);
+        };
+
+        // When this runs, it will add a value to onload
+        fileReader.readAsDataURL(file);
+
+    }
     return (
         <div className={classes.picker}>
             <label htmlFor={name}>{label}</label>
             <div className={classes.controls}>
+                
+                <div className={classes.preview}>
+                    {!pickedImage && <p>No image picked yet.</p>}
+                    {pickedImage && 
+                    <Image src={pickedImage} 
+                    alt="The image selected by the user" fill></Image>}
+                </div>
+                
                 <input 
                 className={classes.input} 
                 type="file" 
                 id={name} 
-                accept="image/png, image/jpeg" 
+                accept="image/png, image/jpeg, image/jpg" 
                 name={name}
                 ref={imageInput}
+                onChange={handleImageChange}
                 ></input>
+                
                 <button 
                 className={classes.button} 
                 type="button" 
