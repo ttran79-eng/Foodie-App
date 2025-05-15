886a0c3815023c0a63a60df1a29238cc5ff8af23
Tin <ttran79@rrc.ca>
2025-05-15 14:27:51 -0500

    Adding user feedback on submit button

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,10 +1,13 @@
 
 import classes from './page.module.css';
 import ImagePicker from '@/components/meals/image-picker';
+import MealsFormSubmit from '@/components/meals/meals-form-submit';
 import { shareMeal } from '@/lib/form_submission_action';
 
+
 export default function ShareMealPage() {
-    
+   
+
   return (
     <>
       <header className={classes.header}>
@@ -44,7 +47,7 @@ export default function ShareMealPage() {
           </p>
           <ImagePicker label="Your image" name="image" />
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
