61e6a55d40cf4f2fa5c03db494bc54d785c546d8
Tin <ttran79@rrc.ca>
2025-05-15 10:37:42 -0500

    Populating meals detail page with db fetching, using {params} to access object

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,8 +1,36 @@
 // localhost:3000/meals/xyz...
 // will direct to this page
+import classes from './page.module.css';
+import Image from 'next/image';
+import { getMeal } from '@/lib/meals';
 
-export default function MealsDetailPage(){
+
+// Slug pages get access to params which contains and object key value
+// Params.mealSlug is the identifier to fetch details 
+
+export default function MealsDetailPage({params}){
+    const meal = getMeal(params.mealSlug)
+    meal.instructions = meal.instructions.replace(/\n/g, '<br />');
+    
     return(
-        <h1>Meals Detail Page</h1>
+        <>
+            <header className={classes.header}>
+                <div className={classes.image}>
+                    <Image src={meal.image} alt={meal.title} fill></Image>
+                </div>
+                <div className={classes.headerText}>
+                    <h1>{meal.title}</h1>
+                    <p className={classes.creator}>
+                        by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+                    </p>
+                    <p className={classes.summary}>{meal.summary}</p>
+                </div>
+            </header>
+            <main>
+                <p className={classes.instructions} dangerouslySetInnerHTML={{
+                    __html: meal.instructions,
+                }}></p>
+            </main>
+        </>
     )
 }
